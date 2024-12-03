8e5dbf0866133dc81550156b61000b99845a268f
sunitarathod1 <sunitarathod0493@gmail.com>
2024-12-03 15:53:54 +0530

    self

---
+++ destructuring/script.jsx
@@ -0,0 +1,15 @@
+const colors=['pink','yellow','orange','green','red']
+
+const[,,a]=colors;
+
+const user ={
+    name:"aaa",
+    age:111,
+    address:{
+        city:"bangalore",
+        state:"karnataka"
+    }
+
+}
+
+const {name ,age } = user
